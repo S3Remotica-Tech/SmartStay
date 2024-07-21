621be31f048f098d5a565310650575ccc346cdd6
Sivasri234 <sivadhanyasri143@gmail.com>
2024-07-21 20:48:11 +0530

    dashboardfont

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -27,7 +27,7 @@ import { createPortal } from 'react-dom';
 import {
   BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,Label,LabelList
 } from 'recharts';
-import { borderRadius } from '@mui/system';
+import { borderRadius, color, fontFamily, fontSize, fontWeight } from '@mui/system';
 
 function Dashboard() {
   const complaintsData = [
@@ -37,7 +37,7 @@ function Dashboard() {
       date: "26 May 2024",
       status: "Pending",
       room: "Room #F205, Bed 1",
-      img: "D:\smartHostel+Update\backend_Smart\smartstay\src\Assets\Images/Group 1.png" 
+      img: "https://randomuser.me/api/portraits/women/2.jpg" 
     },
     {
       name: "Josie Mary",
@@ -86,7 +86,7 @@ function Dashboard() {
   
   
   const formatYAxis = (tickItem) => {
-    return `$${tickItem}`;
+    return `${tickItem}`;
   }
 
   const state = useSelector(state => state)
@@ -182,17 +182,17 @@ function Dashboard() {
     
     const CustomLegend = ({ payload }) => {
       return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center',paddingTop:15}}>
           {payload.map((entry, index) => (
             <div key={`item-${index}`} style={{ display: 'flex', alignItems: 'center', marginRight: 10,marginTop:25 }}>
               <div style={{
-                width: 10,
-                height: 10,
+                width: 12,
+                height: 12,
                 borderRadius: '50%',
                 backgroundColor: entry.color,
                 marginRight: 5
               }} />
-              <span>{entry.value}</span>
+              <span  style={{fontSize:12,fontWeight:600,fontFamily:"Montserrat"}}>{entry.value}</span>
             </div>
           ))}
         </div>
@@ -207,11 +207,11 @@ function Dashboard() {
 
 
 
-<div className='cotainer w-full p-3' >
+<div className='cotainer  p-4' >
 <div className='texxttt'>
   <div style={{flex:1}}>
-<div style={{font:"Gilroy",fontWeight:600,fontSize:"24px",lineHeight:"28.63px"}}>Welcome back, Vikram!</div>
-<div>Manage all the inventory and analytics form here</div>
+<lable style={{fontFamily:"Montserrat",fontWeight:600,fontSize:"24px",lineHeight:"28.63px"}}>Welcome back, Vikram!</lable>
+<p style={{fontFamily:"Montserrat",fontWeight:400,fontSize:"16px",color:"#4B4B4B"}} >Manage all the inventory and analytics form here</p>
   </div>
   <div style={{flex:1}}>
   <div className="headerone">
@@ -233,77 +233,77 @@ function Dashboard() {
   </div>
 
 
-<div className="row " style={{marginTop:30,width:"100%"}}>
+{/* <div className="row carddesign" >
       
-      <div className="col-lg-3 col-md-3 col-sm-12 col-12 mb-3">
-  <Card style={{ height: "auto",width:"100%" }}>
+      <div className="col-lg-3 col-md-12 col-sm-12 col-xs-12 mb-3">
+  <Card style={{ height: "auto",width:"100%",borderRadius:"20px" }}>
     <Card.Body>
       <div >
         <div >
         <img 
             src={vector} 
-            height={35} 
-            width={35} 
+            height={32} 
+            width={32} 
             
           />
-          <h6 style={{paddingTop:15}}>Total Hostel</h6>
+          <p style={{paddingTop:15,fontSize:16,fontWeight:400,fontFamily:"Montserrat"}}>Total Hostel</p>
         
         </div>
-        <div  style={{fontSize:32,fontWeight:600,font:"Gilroy"}}>
+        <p style={{fontSize:32,fontWeight:600,fontFamily:"Montserrat"}}>
           {hostelCount.toLocaleString()}
-        </div>
+        </p>
       </div>
     </Card.Body>
   </Card>
 </div>
 
-<div className="col-lg-3 col-md-3 col-sm-12 col-12 mb-3">
-  <Card style={{ height: "auto",width:"100%" }}>
+<div className="col-lg-3 col-md-3 col-sm-12 col-xs-12 mb-3">
+  <Card style={{ height: "auto",width:"100%",borderRadius:"20px" }}>
     <Card.Body>
 
       <div >
         <div >
         <img 
             src={key} 
-            height={35} 
-            width={35} 
+            height={32} 
+            width={32} 
             
           />
-          <h6 style={{paddingTop:15}}>Available Beds</h6>
+          <p style={{paddingTop:15,fontSize:16,fontWeight:400,fontFamily:"Montserrat"}}>Available Beds</p>
         
         </div> 
-        <div style={{fontSize:32,fontWeight:600,font:"Gilroy"}}>
+        <p style={{fontSize:32,fontWeight:600,fontFamily:"Montserrat"}}>
           {availableBed.toLocaleString()}
-        </div>
+        </p>
       </div>
     </Card.Body>
   </Card>
 </div>
 
-<div className="col-lg-3 col-md-3 col-sm-12 col-12 mb-3">
-  <Card style={{ height: "auto",width:"100%" }}>
+<div className="col-lg-3 col-md-3 col-sm-12 col-xs-12 mb-3">
+  <Card style={{ height: "auto",width:"100%",borderRadius:"20px" }}>
     <Card.Body>
       <div >
         <div >
         <img 
             src={clock} 
-            height={35} 
-            width={35} 
+            height={32} 
+            width={32} 
            
           />
-          <h6 style={{paddingTop:15}}>Total Rooms</h6>
+          <p style={{paddingTop:15,fontSize:16,fontWeight:400,fontFamily:"Montserrat"}}>Total Rooms</p>
         
         </div>
-        <div style={{fontSize:32,fontWeight:600,font:"Gilroy"}}>
+        <p style={{fontSize:32,fontWeight:600,font:"Montserrat"}}>
           {roomCount.toLocaleString()}
-        </div>
+        </p>
       </div>
     </Card.Body>
   </Card>
 </div>
 
-<div className="col-lg-3 col-md-3 col-sm-12 col-12 mb-3">
-  <Card style={{ height: "auto",width:"100%" }}>
+<div className="col-lg-3 col-md-3 col-sm-12 col-xs-12 mb-3">
+  <Card style={{ height: "auto",width:"100%",borderRadius:"20px" }}>
     <Card.Body>
       <div >
         <div >
@@ -313,12 +313,12 @@ function Dashboard() {
             width={35} 
             
           />
-          <h6 style={{paddingTop:15}}>Occupied Beds</h6>
+          <p style={{paddingTop:15,fontSize:16,fontWeight:400,fontFamily:"Montserrat"}}>Occupied Beds</p>
         
         </div>
-        <div style={{fontSize:32,fontWeight:600,font:"Gilroy"}}>
+        <p style={{fontSize:32,fontWeight:600,font:"Montserrat"}}>
           {occupied_Bed.toLocaleString()}
-        </div>
+        </p>
       </div>
     </Card.Body>
   </Card>
@@ -330,21 +330,89 @@ function Dashboard() {
 
 
 
-      </div>
+      </div> */}
+    
+  <div className="row carddesign">
+    <div className="col-lg-4 col-md-12 col-sm-12 col-xl-3 mb-3">
+      <Card style={{ height: "auto", width: "100%", borderRadius: "20px" }}>
+        <Card.Body>
+          <div>
+            <div>
+              <img src={vector} height={32} width={32} />
+              <p style={{ paddingTop: 15, fontSize: 16, fontWeight: 400, fontFamily: "Montserrat" }}>Total Hostel</p>
+            </div>
+            <p style={{ fontSize: 32, fontWeight: 600, fontFamily: "Montserrat" }}>
+              {hostelCount.toLocaleString()}
+            </p>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+
+    <div className="col-lg-3 col-md-6 col-sm-12 mb-3">
+      <Card style={{ height: "auto", width: "100%", borderRadius: "20px" }}>
+        <Card.Body>
+          <div>
+            <div>
+              <img src={key} height={32} width={32} />
+              <p style={{ paddingTop: 15, fontSize: 16, fontWeight: 400, fontFamily: "Montserrat" }}>Available Beds</p>
+            </div>
+            <p style={{ fontSize: 32, fontWeight: 600, fontFamily: "Montserrat" }}>
+              {availableBed.toLocaleString()}
+            </p>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+
+    <div className="col-lg-3 col-md-6 col-sm-12 mb-3">
+      <Card style={{ height: "auto", width: "100%", borderRadius: "20px" }}>
+        <Card.Body>
+          <div>
+            <div>
+              <img src={clock} height={32} width={32} />
+              <p style={{ paddingTop: 15, fontSize: 16, fontWeight: 400, fontFamily: "Montserrat" }}>Total Rooms</p>
+            </div>
+            <p style={{ fontSize: 32, fontWeight: 600, fontFamily: "Montserrat" }}>
+              {roomCount.toLocaleString()}
+            </p>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+
+    <div className="col-lg-3 col-md-6 col-sm-12 mb-3">
+      <Card style={{ height: "auto", width: "100%", borderRadius: "20px" }}>
+        <Card.Body>
+          <div>
+            <div>
+              <img src={car} height={35} width={35} />
+              <p style={{ paddingTop: 15, fontSize: 16, fontWeight: 400, fontFamily: "Montserrat" }}>Occupied Beds</p>
+            </div>
+            <p style={{ fontSize: 32, fontWeight: 600, fontFamily: "Montserrat" }}>
+              {occupied_Bed.toLocaleString()}
+            </p>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+  </div>
+
+
 <div className='circulardes' >
 
 <div className='w-full' style={{flex:1}}>
 
 
-<div className='crddesg w-full' style={{ padding: '0px',paddingTop:"20px", border: '1px solid #e0e0e0', borderRadius: '8px', backgroundColor: '#fff', maxWidth: '520px', }}>
+<div className='crddesg w-full' style={{ padding: '0px',paddingTop:"20px", border: '1px solid #e0e0e0', borderRadius: '20px', backgroundColor: '#fff', }}>
   <div className='dropp' style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10 20px' }}>
-    <div style={{ display: 'flex', textAlign: 'start',fontSize:18,fontWeight:600 }}>
-      Revenus vs Expenses
+    <div style={{ display: 'flex', textAlign: 'start' }}>
+      <p style={{fontFamily:"Montserrat",fontSize:18,fontWeight:600,paddingLeft:10}}>Revenus vs Expenses</p>
     </div>
     <div className="d-flex align-items-end mb-3 justify-content-end" style={{marginTop:10}}>
       <div className="dropdown">
         <button className="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style={{ color: 'black', borderColor: 'grey' }}>
-          2024 - 2025
+         <span style={{fontFamily:"Montserrat",fontSize:12,fontWeight:600}}> 2024 - 2025</span>
         </button>
         <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
           <a className="dropdown-item" href="#">Action</a>
@@ -358,8 +426,8 @@ function Dashboard() {
 <div style={{ position: 'relative', width: '100%', height: 350 }}>
     <div style={{ position: 'absolute', top: '10px', left: '20px', display: 'flex', alignItems: 'center' }}>
       <img src={arrow} alt="Arrow" style={{ width: '10px', height: '30px', marginLeft: '10px' }} />
-      <div style={{ transform: 'rotate(-90deg)', transformOrigin: 'left center', marginTop: '180px', color: '#000' }}>
-      <p className='me-3'>  Amount</p>
+      <div style={{ transform: 'rotate(-90deg)', transformOrigin: 'left center', marginTop: '150px', color: '#000' }}>
+      <p className='me-3' style={{fontFamily:"Montserrat",fontWeight:600,fontSize:12,color:"#4B4B4B"}}>  Amount</p>
       </div>
     </div>
     {/* {
@@ -377,13 +445,13 @@ function Dashboard() {
         barCategoryGap="10%"
       >
         <CartesianGrid horizontal vertical={false} stroke="#e0e0e0" />
-        <XAxis dataKey="month" style={{ marginTop: "20px" }}>
-          <Label value="" position="insideBottom" offset={-15} />
+        <XAxis dataKey="month" style={{ marginTop: "20px" }}  tick={{ fontFamily: 'Gilroy', fontSize: 12, fontWeight: 500 }}>
+          <Label value="" position="insideBottom" offset={-15}  />
         </XAxis>
-        <YAxis axisLine={false} tickLine={false} tickFormatter={formatYAxis} dx={-10} />
-        <Tooltip formatter={(value) => `$${value}`} />
+        <YAxis axisLine={false} tickLine={false} tickFormatter={formatYAxis} dx={-10}  tick={{ fontFamily: 'Gilroy', fontSize: 12, fontWeight: 500 }} />
+        <Tooltip formatter={(value) => `${value}`} />
         <Bar dataKey="revenue" fill="#1E45E1" barSize={24} radius={[5, 5, 0, 0]}>
-          <LabelList dataKey={data.revenue} position="top" style={{ fill: '#1E45E1' }} />
+          <LabelList dataKey="Revenue" position="top" style={{ fill: '#1E45E1' }} />
         </Bar>
         <Bar dataKey="Expenses" fill="#00C2FF" barSize={24} radius={[5, 5, 0, 0]}>
           <LabelList dataKey="ExpensesLabel" position="top" style={{ fill: '#00C2FF' }} />
@@ -396,18 +464,22 @@ function Dashboard() {
       <img src={leftarrow} alt="Arrow" style={{ width: '30px', height: '10px' }} />
     </div> */}
     <div style={{ position: 'absolute', bottom: '50px', left: '50%', transform: 'translateX(-50%)', display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
-      <span style={{ marginRight: '5px' }}>Month</span>
+      <span style={{ marginRight: '5px',fontFamily:"Montserrat",fontWeight:600,fontSize:12,color:"#4B4B4B" }}>Month</span>
       <img src={leftarrow} alt="Arrow" style={{ width: '30px', height: '10px' }} />
+      
     </div>
+
+
+  
   </div>
  
 </div>
 
-<Card  style={{ marginTop:15,height:"28%",width:"94%"}}>
+<Card  style={{ marginTop:15,height:"28%",width:"97%",borderRadius:"20px"}}>
   <Card.Body className="d-flex flex-column align-items-start">
-    <div  style={{fontSize:"18px",fontWeight:600,font:"Montserrat"}}>Revenue Target</div>
+    <div  style={{fontSize:"18px",fontWeight:600,fontFamily:"Montserrat",paddingLeft:10}}>Revenue Target</div>
     <div className='circulardesone' >
-      <div className= 'circular-progressbar-container' style={{ width: '40%',marginLeft:30,marginTop:10 }}>
+      <div className= 'circular-progressbar-container' style={{ width: '35%',marginLeft:30,marginTop:10 }}>
       <CircularProgressbar
    
      value={percentage}
@@ -415,15 +487,20 @@ function Dashboard() {
      circleRatio={0.5}
      styles={buildStyles({
        rotation: 0.75,
-       textSize: '15px',
        pathColor: '#00A32E',
        trailColor: '#EBEBEB',
        textColor: '#000000',
+       textSize:15,
+       
       //  width:"70%",
        text: {
          fill: '#0000000',
+       
          transform: 'rotate(80deg)',
          transformOrigin: 'center center',
+         fontFamily:"Gilroy",
+         fontWeight:600,
+         fontSize:"24px",
        },
      })}
    />
@@ -433,15 +510,15 @@ function Dashboard() {
   <div className="status-item">
     <div className="dot received"></div>
     <div>
-      <div>Received</div>
-      <div>₹{current.toLocaleString()}</div>
+      <div style={{fontSize:16,fontWeight:600,fontFamily:"Montserrat"}}>Received</div>
+      <div style={{fontSize:16,fontWeight:600,fontFamily:"Gilry"}}>₹{current.toLocaleString()}</div>
     </div>
   </div>
   <div className="status-item">
     <div className="dot receivable"></div>
-    <div>
-      <div>Receivable</div>
-      <div>₹{overdue.toLocaleString()}</div>
+    <div style={{marginTop:10}}>
+      <div style={{fontSize:16,fontWeight:600,fontFamily:"Montserrat"}}>Receivable</div>
+      <div style={{fontSize:16,fontWeight:600,fontFamily:"Gilry"}}>₹{overdue.toLocaleString()}</div>
     </div>
   </div>
 </div>
@@ -458,20 +535,20 @@ function Dashboard() {
 
 <div className="expenses-container">
       <div className="headertwo">
-        <h2>Expenses Breakdown</h2>
+        <p style={{fontFamily:"Montserrat",fontSize:18,fontWeight:600}}>Expenses Breakdown</p>
       </div>
       <div className="content">
         <div className="chart">
-          <Doughnut data={datum} options={options} />
-          <div className="center-text">₹19,500</div>
+          <Doughnut data={datum} options={options} style={{width:196,height:196}} />
+          <p className="center-text" style={{fontFamily:"Gilroy",fontSize:25,fontWeight:600}}>₹19,500</p>
         </div>
         <div className="categories">
           {labels.map((label, index) => (
             <div className="category" key={index}>
               <span className="dot" style={{ backgroundColor: datasets[0].backgroundColor[index] }}></span>
               <div className="text">
-                <p>{label}</p>
-                <p>₹19,500</p>
+                <p style={{fontFamily:"Montserrat",fontSize:12,fontWeight:600,color:"#4B4B4B"}}>{label}</p>
+                <p style={{fontFamily:"Gilroy",fontSize:16,fontWeight:600}}>₹19,500</p>
               </div>
             </div>
           ))}
@@ -480,21 +557,21 @@ function Dashboard() {
     </div>
     <div className="complaints-container">
   <div className="header">
-    <h2>Active Complaints</h2>
-    <a href="/view-all" style={{ paddingRight: 20 }}>View all</a>
+    <p style={{fontSize:18,fontWeight:600,fontFamily:"Montserrat",paddingLeft:"10px",marginTop:15}}>Active Complaints</p>
+    <a href="/view-all" style={{ textAlign:"right",paddingRight:15 }}>View all</a>
   </div>
   {complaintsData.map((complaint, index) => (
-    <div className="complaint" key={index}>
+    <div className="complaint" key={index} >
       <img src={complaint.img} alt={complaint.name} className="avatar" />
       <div className="complaint-info">
-        <p className="name">{complaint.name}</p>
-        <p className="details">{complaint.type} . {complaint.date}</p>
+        <p className="name" style={{fontSize:16,fontFamily:"Gilroy",fontWeight:600}} >{complaint.name}</p>
+        <p className="details" style={{fontSize:14,fontFamily:"Gilroy",fontWeight:500,color:"#4B4B4B"}}>{complaint.type} . {complaint.date}</p>
       </div>
       <div>
         <div className={`status ${complaint.status.toLowerCase()}`}>
-          {complaint.status}
+         <p style={{fontSize:14,fontFamily:"Gilroy",fontWeight:500,color:"#222222",paddingTop:3,paddingBottom:3,paddingLeft:2,paddingRight:2,marginBottom:"0px"}}> {complaint.status}</p>
         </div>
-        <div className="room">{complaint.room}</div>
+        <div className="room" style={{fontSize:14,fontFamily:"Gilroy",fontWeight:500,color:"#4B4B4B",paddingRight:"10px"}}>{complaint.room}</div>
       </div>
     </div>
   ))}
