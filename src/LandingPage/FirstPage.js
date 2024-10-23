f68f89acac51c4af29712fc74fd61bbf865e2bb4
hepziyal <annai01021998@gmail.com>
2024-10-23 16:51:10 +0530

    LandigPage

--- src/LandingPage/FirstPage.js
+++ src/LandingPage/FirstPage.js
@@ -1,6 +1,6 @@
-import React , { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
-import Desktop  from '../Assets/Images/New_images/image 8.png'
+import Desktop from '../Assets/Images/New_images/image 8.png'
 import { Height } from '@material-ui/icons';
 import Spring1 from '../Assets/Images/New_images/image 24.png'
 import Spring2 from '../Assets/Images/New_images/image 23.png';
@@ -9,50 +9,50 @@ import CountUp from 'react-countup';
 
 function FirstPage() {
 
-      useEffect(() => {
-        window.scrollTo(0, 0);
-      }, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
 
-    let navigate = useNavigate();
+  let navigate = useNavigate();
 
-    const handleStartNow = () =>{
-        navigate('/create-account')
-      } 
+  const handleStartNow = () => {
+    navigate('/create-account')
+  }
 
-    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
-    useEffect(() => {
-        const handleResize = () => {
-          setIsMobile(window.innerWidth <= 768);
-        };
-    
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-      }, []);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
 
 
-      useEffect(() => {
-        const appearOptions = {
-          threshold : 0.5
-        };
-        const faders = document.querySelectorAll('.fade-in'); 
-        const appearOnScro1l = new IntersectionObserver(function(entries,appearOnScrool){
-          entries.forEach(entry =>{
-            if(!entry.isIntersecting){
-              return;
-            }
-            else{
-              entry.target.classList.add('appear');
-              appearOnScro1l.unobserve(entry.target);
-            }
-          })
-        }, appearOptions)
-        faders.forEach(fader =>{
-          appearOnScro1l.observe(fader);
-        })
-      });
+  useEffect(() => {
+    const appearOptions = {
+      threshold: 0.5
+    };
+    const faders = document.querySelectorAll('.fade-in');
+    const appearOnScro1l = new IntersectionObserver(function (entries, appearOnScrool) {
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+          return;
+        }
+        else {
+          entry.target.classList.add('appear');
+          appearOnScro1l.unobserve(entry.target);
+        }
+      })
+    }, appearOptions)
+    faders.forEach(fader => {
+      appearOnScro1l.observe(fader);
+    })
+  });
 
 
 
@@ -61,40 +61,38 @@ function FirstPage() {
 
   return (
     <>
-    
-
-    <div style={{backgroundColor:"#FFFFFF" ,height:"",paddingTop:40,width:"100%"}} className='container animated-text'>
-
-<div className='d-flex justify-content-center align-items-center' style={{ textAlign: 'center' }}>
-<label>👋   <span style={{ fontFamily: "Montserrat", color: "rgba(0, 0, 0, 1)", fontWeight: 600, fontSize:18 }}>MANAGE YOUR PG</span></label>
-
-</div>
-<div className='d-flex justify-content-center align-items-center pb-3' style={{ textAlign: 'center',wordSpacing:1 }}>
-    <label style={{ fontSize: isMobile ? 40 : 70, wordSpacing:1 }}>
-        <span style={{ fontFamily: "Gilroy", color: "rgba(30, 69, 225, 1)", fontWeight: 600 }}>Simplify </span>
-        <span style={{ fontWeight: 400, fontFamily: "Gilroy" }}>your </span>{''}
-        <span style={{ fontFamily: "Gilroy", color: "rgba(0, 0, 0, 1)", fontWeight: 600 }}>Paying Guest management </span>
-        <span style={{ fontWeight: 400, fontFamily: "Gilroy" }}>with </span>
-        <span style={{ color: "rgba(30, 69, 225, 1)", fontWeight: 700, fontFamily: "Gilroy" }}>SmartStay</span>
-    </label>
-</div>
-
-<div className='d-flex justify-content-center mb-1' >
-    <label style={{fontWeight:400, fontSize:18, color:"rgba(0, 0, 0, 1)", fontFamily:"Montserrat", textAlign:"center"}}>Efficiently manage rooms, customers, inventory, vendors, 
-        </label>
-</div>
-<div className='d-flex justify-content-center mb-1 '>
-<label style={{fontWeight:400, fontSize:18, color:"rgba(0, 0, 0, 1)", fontFamily:"Montserrat",textAlign:"center"}}>complaints, expenses, and reports—all in one place.</label>
-
-</div>
 
 
-<div className='d-flex justify-content-center mt-3 '>
-<Button onClick={handleStartNow} variant="" style={{backgroundColor:"rgba(30, 69, 225, 1)", color:"rgba(255, 255, 255, 1)", fontWeight:600, fontSize:16, fontFamily:"Montserrat", width:320,height:70, borderRadius:16, padding:"24px 32px 24px 32px"}}>Get Started</Button>
+      <div style={{ backgroundColor: "#FFFFFF", height: "", paddingTop: 40, width: "100%" }} className='container animated-text'>
 
-</div>
+        <div className='d-flex justify-content-center align-items-center' style={{ textAlign: 'center' }}>
+          <label>👋   <span style={{ fontFamily: "Montserrat", color: "rgba(0, 0, 0, 1)", fontWeight: 600, fontSize: 18, lineHeight: "28.8px" }}>MANAGE YOUR PG</span></label>
+
+        </div>
+        <div className='d-flex justify-content-center align-items-center pb-3' style={{ textAlign: 'center', wordSpacing: 1 }}>
+          <label style={{ fontSize: isMobile ? 40 : 70, wordSpacing: 1 }}>
+            <span style={{ fontFamily: "Gilroy", color: "rgba(30, 69, 225, 1)", fontWeight: 600 }}>Simplify </span>
+            <span style={{ fontWeight: 300, fontFamily: "Gilroy" }}>your </span>{''}
+            <span style={{ fontFamily: "Gilroy", color: "rgba(0, 0, 0, 1)", fontWeight: 500 }}>Paying Guest management </span>
+            <span style={{ fontWeight: 300, fontFamily: "Gilroy" }}>with </span>
+            <span style={{ color: "rgba(30, 69, 225, 1)", fontWeight: 600, fontFamily: "Gilroy" }}>SmartStay</span>
+          </label>
+        </div>
+
+        <div className='d-flex justify-content-center mb-1' style={{paddingTop:"36px"}} >
+          <label style={{ fontWeight: 400, fontSize: 18, color: "rgba(0, 0, 0, 1)", fontFamily: "Montserrat", textAlign: "center" }}>Efficiently manage rooms, customers, inventory, vendors,
+          </label>
+        </div>
+        <div className='d-flex justify-content-center mb-1 '>
+          <label style={{ fontWeight: 400, fontSize: 18, color: "rgba(0, 0, 0, 1)", fontFamily: "Montserrat", textAlign: "center" }}>complaints, expenses, and reports—all in one place.</label>
+
+        </div>
 
 
+        <div className='d-flex justify-content-center mt-3 ' style={{ paddingTop: "24px" }}>
+          <Button onClick={handleStartNow} variant="" style={{ backgroundColor: "rgba(30, 69, 225, 1)", color: "rgba(255, 255, 255, 1)", fontWeight: 600, fontSize: 16, fontFamily: "Montserrat", width: 320, height: 70, borderRadius: 16, padding: "24px 53px 24px 53px" }}>Get Started</Button>
+
+        </div>
 
 
 
@@ -103,31 +101,33 @@ function FirstPage() {
 
 
 
-</div>
-<div className='row d-flex justify-content-center mt-4' style={{position:"relative", backgroundColor:""}}>
-       <img src={Desktop} className='img-fluid' style={{height:776, width:1000}}/>
-       <img src={Spring1}  style={{position:"absolute", left:0, bottom:0, width:220, height:219 ,display:isMobile ? "none": "flex",transform: "rotate(360deg)"}}/>
-       <img src={Spring2}   style={{position:"absolute", top:0, right:0, width:220, height:219,display:isMobile ? "none": "flex"}} />
 
-</div>
 
-<div className="d-flex justify-content-around flex-wrap mt-5 mb-5" style={{backgroundColor:"rgba(250, 249, 255, 1)",padding:"20px 10px"}}>
+      </div>
+      <div className='row d-flex justify-content-center mt-4' style={{ position: "relative", backgroundColor: "" }}>
+        <img src={Desktop} className='img-fluid' style={{ height: 776, width: 1000 }} />
+        <img src={Spring1} style={{ position: "absolute", left: 0,top:600, bottom:0, width: 220, height: 219, display: isMobile ? "none" : "flex", transform: "rotate(360deg)" }} />
+        <img src={Spring2} style={{ position: "absolute", top:-30, right: 0,bottom:0, width: 220, height: 219, display: isMobile ? "none" : "flex" }} />
 
-<div className='text-center'>
-    <div style={{fontSize:60,fontWeight:700,fontFamily:"Montserrat",color:"rgba(30, 69, 225, 1)"}}> <CountUp start={1} end={200} delay={2}/>K</div>
-    <div style={{fontSize:18,fontWeight:400,fontFamily:"Montserrat",color:"rgba(0, 0, 0, 1)"}}>Users use this platform</div>
-</div>
+      </div>
 
-<div className='text-center'>
-    <div style={{fontSize:60,fontWeight:700,fontFamily:"Montserrat",color:"rgba(30, 69, 225, 1)"}}><CountUp start={1} end={30} delay={2}/>K+</div>
-    <div style={{fontSize:18,fontWeight:400,fontFamily:"Montserrat",color:"rgba(0, 0, 0, 1)"}}>Active users this month</div>
-</div>
-<div className='text-center'>
-    <div style={{fontSize:60,fontWeight:700,fontFamily:"Montserrat",color:"rgba(30, 69, 225, 1)"}}><CountUp start={1} end={25} delay={2}/>K+</div>
-    <div style={{fontSize:18,fontWeight:400,fontFamily:"Montserrat",color:"rgba(0, 0, 0, 1)"}}>Available states</div>
-</div>
-</div>
-</>
+      <div className="d-flex justify-content-around flex-wrap mt-5 mb-5" style={{ backgroundColor: "rgba(250, 249, 255, 1)", padding: "42px 10px 42px 10px" }}>
+
+        <div className='text-center'>
+          <div style={{ fontSize: 60, fontWeight: 800, fontFamily: "Montserrat", color: "rgba(30, 69, 225, 1)" }}> <CountUp start={1} end={200} delay={2} />K</div>
+          <div style={{ fontSize: 18, fontWeight: 400, fontFamily: "Montserrat", color: "rgba(0, 0, 0, 1)" }}>Users use this platform</div>
+        </div>
+
+        <div className='text-center'>
+          <div style={{ fontSize: 60, fontWeight: 800, fontFamily: "Montserrat", color: "rgba(30, 69, 225, 1)" }}><CountUp start={1} end={30} delay={2} />K+</div>
+          <div style={{ fontSize: 18, fontWeight: 400, fontFamily: "Montserrat", color: "rgba(0, 0, 0, 1)" }}>Active users this month</div>
+        </div>
+        <div className='text-center'>
+          <div style={{ fontSize: 60, fontWeight: 800, fontFamily: "Montserrat", color: "rgba(30, 69, 225, 1)" }}><CountUp start={1} end={25} delay={2} />K+</div>
+          <div style={{ fontSize: 18, fontWeight: 400, fontFamily: "Montserrat", color: "rgba(0, 0, 0, 1)" }}>Available states</div>
+        </div>
+      </div>
+    </>
   )
 }
 
