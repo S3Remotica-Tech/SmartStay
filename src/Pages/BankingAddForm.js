c9fd96d854a3642b9a38e0b27adbb92bf72fa787
Sivasri234 <sivadhanyasri143@gmail.com>
2024-10-29 18:04:13 +0530

    bank

--- src/Pages/BankingAddForm.js
+++ src/Pages/BankingAddForm.js
@@ -1,9 +1,292 @@
-import React from "react";
+import Modal from "react-bootstrap/Modal";
+import { FormControl } from "react-bootstrap";
+import React, { useEffect, useState, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-function BankingAddForm(){
+
+
+
+
+
+function BankingAddForm(props){
+    const handleClose=()=>{
+        props.setShowForm(false)
+    }
     return(
         <div>
-            BankingAddForm
+             <Modal
+          show={props.showForm}
+          onHide={() => handleClose()}
+          backdrop="static"
+          centered
+        >
+          {/* <Modal.Header closeButton className="text-center">
+            <Modal.Title style={{ fontSize: 18,fontFamily:"Gilroy",fontWeight:600 }} className="text-center">
+              Add a Reading
+            </Modal.Title>
+          </Modal.Header> */}
+
+<Modal.Header
+                    style={{ marginBottom: "30px", position: "relative" }}
+                  >
+                    <div
+                      style={{
+                        fontSize: 20,
+                        fontWeight: 600,
+                        fontFamily: "Gilroy",
+                      }}
+                    >
+                      Add bank
+                    </div>
+                    <button
+                      type="button"
+                      className="close"
+                      aria-label="Close"
+                      onClick={handleClose}
+                      style={{
+                        position: "absolute",
+                        right: "10px",
+                        top: "16px",
+                        border: "1px solid black",
+                        background: "transparent",
+                        cursor: "pointer",
+                        padding: "0",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        width: "32px",
+                        height: "32px",
+                        borderRadius: "50%",
+                      }}
+                    >
+                      <span
+                        aria-hidden="true"
+                        style={{
+                          fontSize: "30px",
+                          paddingBottom: "6px",
+                        }}
+                      >
+                        &times;
+                      </span>
+                    </button>
+                  </Modal.Header>
+          <Modal.Body>
+            <div className="row ">
+            <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <Form.Group className="mb-3">
+                  <Form.Label
+                    style={{
+                      fontSize: 14,
+                      color: "#222222",
+                      fontFamily: "Gilroy",
+                      fontWeight: 500,
+                    }}
+                  >
+                    Account name{" "}
+                    <span style={{ color: "red", fontSize: "20px" }}> * </span>
+                  </Form.Label>
+                  <FormControl
+                    type="text"
+                    id="form-controls"
+                    placeholder="Enter account name"
+                    // value={endmeter}
+                    // onChange={(e) => handleendmeter(e)}
+                    style={{
+                      fontSize: 16,
+                      color: "#4B4B4B",
+                      fontFamily: "Gilroy",
+                      fontWeight: 500,
+                      boxShadow: "none",
+                      border: "1px solid #D9D9D9",
+                      height: 50,
+                      borderRadius: 8,
+                    }}
+                  />
+                </Form.Group>
+                {/* {endMeterError && (
+                  <div style={{ color: "red" }}>
+                    <MdError />
+                    {endMeterError}
+                  </div>
+                )} */}
+              </div>
+
+              <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <Form.Group className="mb-3">
+                  <Form.Label
+                    style={{
+                      fontSize: 14,
+                      color: "#222222",
+                      fontFamily: "Gilroy",
+                      fontWeight: 500,
+                    }}
+                  >
+                    Account no.{" "}
+                    <span style={{ color: "red", fontSize: "20px" }}> * </span>
+                  </Form.Label>
+                  <FormControl
+                    type="text"
+                    id="form-controls"
+                    placeholder="Enter account no."
+                    // value={endmeter}
+                    // onChange={(e) => handleendmeter(e)}
+                    style={{
+                      fontSize: 16,
+                      color: "#4B4B4B",
+                      fontFamily: "Gilroy",
+                      fontWeight: 500,
+                      boxShadow: "none",
+                      border: "1px solid #D9D9D9",
+                      height: 50,
+                      borderRadius: 8,
+                    }}
+                  />
+                </Form.Group>
+                {/* {endMeterError && (
+                  <div style={{ color: "red" }}>
+                    <MdError />
+                    {endMeterError}
+                  </div>
+                )} */}
+              </div>
+              <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <Form.Group className="mb-3">
+                  <Form.Label
+                    style={{
+                      fontSize: 14,
+                      color: "#222222",
+                      fontFamily: "Gilroy",
+                      fontWeight: 500,
+                    }}
+                  >
+                    Bank Name{" "}
+                    <span style={{ color: "red", fontSize: "20px" }}> * </span>
+                  </Form.Label>
+                  <FormControl
+                    type="text"
+                    id="form-controls"
+                    placeholder="Enter bank name"
+                    // value={endmeter}
+                    // onChange={(e) => handleendmeter(e)}
+                    style={{
+                      fontSize: 16,
+                      color: "#4B4B4B",
+                      fontFamily: "Gilroy",
+                      fontWeight: 500,
+                      boxShadow: "none",
+                      border: "1px solid #D9D9D9",
+                      height: 50,
+                      borderRadius: 8,
+                    }}
+                  />
+                </Form.Group>
+                {/* {endMeterError && (
+                  <div style={{ color: "red" }}>
+                    <MdError />
+                    {endMeterError}
+                  </div>
+                )} */}
+              </div>
+
+              <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <Form.Group className="mb-3">
+                  <Form.Label
+                    style={{
+                      fontSize: 14,
+                      color: "#222222",
+                      fontFamily: "Gilroy",
+                      fontWeight: 500,
+                    }}
+                  >
+                    IFSC code{" "}
+                    <span style={{ color: "red", fontSize: "20px" }}> * </span>
+                  </Form.Label>
+                  <FormControl
+                    type="text"
+                    id="form-controls"
+                    placeholder="Enter IFSC code"
+                    // value={endmeter}
+                    // onChange={(e) => handleendmeter(e)}
+                    style={{
+                      fontSize: 16,
+                      color: "#4B4B4B",
+                      fontFamily: "Gilroy",
+                      fontWeight: 500,
+                      boxShadow: "none",
+                      border: "1px solid #D9D9D9",
+                      height: 50,
+                      borderRadius: 8,
+                    }}
+                  />
+                </Form.Group>
+                {/* {endMeterError && (
+                  <div style={{ color: "red" }}>
+                    <MdError />
+                    {endMeterError}
+                  </div>
+                )} */}
+              </div>
+              <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <Form.Group className="mb-3">
+                  <Form.Label
+                    style={{
+                      fontSize: 14,
+                      color: "#222222",
+                      fontFamily: "Gilroy",
+                      fontWeight: 500,
+                    }}
+                  >
+                    Description{" "}
+                    <span style={{ color: "red", fontSize: "20px" }}> * </span>
+                  </Form.Label>
+                  <FormControl
+                    type="text"
+                    id="form-controls"
+                    placeholder="Enter description"
+                    // value={endmeter}
+                    // onChange={(e) => handleendmeter(e)}
+                    style={{
+                      fontSize: 16,
+                      color: "#4B4B4B",
+                      fontFamily: "Gilroy",
+                      fontWeight: 500,
+                      boxShadow: "none",
+                      border: "1px solid #D9D9D9",
+                      height: 50,
+                      borderRadius: 8,
+                    }}
+                  />
+                </Form.Group>
+                {/* {endMeterError && (
+                  <div style={{ color: "red" }}>
+                    <MdError />
+                    {endMeterError}
+                  </div>
+                )} */}
+              </div>
+              
+            </div>
+          </Modal.Body>
+          <Modal.Footer className="d-flex justify-content-center">
+            <Button
+              className="col-lg-6 col-md-6 col-sm-12 col-xs-12"
+              style={{
+                backgroundColor: "#1E45E1",
+                fontWeight: 600,
+                height: 50,
+                borderRadius: 12,
+                fontSize: 16,
+                fontFamily: "Montserrat, sans-serif",
+                marginTop: 20,
+              }}
+            //   onClick={handleSaveEbBill}
+            >
+              Add Bank
+            </Button>
+          </Modal.Footer>
+        </Modal>
         </div>
     )
 }
