38c94c20053f5613e41498d599280264ba7260b8
Allwin <allwinpon@gmail.com>
2024-12-11 12:50:50 +0530

    conflict clear  completed

--- src/Redux/Action/SettingsAction.js
+++ src/Redux/Action/SettingsAction.js
@@ -22,41 +22,36 @@ export async function AddExpencesCategory(Expences) {
     })
   }  
 
-  export async function DeleteExpencesCategoryList(expences) {
-    return await AxiosConfig.post('/delete/delete-category', expences, {
-      data: expences
-    })
-  }
+  // export async function DeleteExpencesCategoryList(expences) {
+  //   return await AxiosConfig.post('/delete/delete-category', expences, {
+  //     data: expences
+  //   })
+  // }
 
-  export async function Addcomplainttype(type) {
-    console.log("Expences",type);
-    return await AxiosConfig.post('/complaint_types',type,{
-      data:type
-    })
-  }
-
-
-  export async function Complainttypelist() {
-    return await AxiosConfig.get('/all_complaint_types',{
-    })
-  }  
-
-  export async function DeletecomplaintType(types) {
-    return await AxiosConfig.post('/remove_complaint_type', types, {
-      data: types
-    })
-  }
+  // export async function Addcomplainttype(type) {
+  //   console.log("Expences",type);
+  //   return await AxiosConfig.post('/complaint_types',type,{
+  //     data:type
+  //   })
+  // }
 
 
-  console.log("Expences", Expences);
-  return await AxiosConfig.post("/add/expense-category", Expences, {
-    data: Expences,
-  });
-}
+  // export async function Complainttypelist() {
+  //   return await AxiosConfig.get('/all_complaint_types',{
+  //   })
+  // }  
 
-export async function ExpencesCategorylist() {
-  return await AxiosConfig.post("/get/expense-category", {});
-}
+  // export async function DeletecomplaintType(types) {
+  //   return await AxiosConfig.post('/remove_complaint_type', types, {
+  //     data: types
+  //   })
+  // }
+
+ 
+
+// export async function ExpencesCategorylist() {
+//   return await AxiosConfig.post("/get/expense-category", {});
+// }
 
 export async function DeleteExpencesCategoryList(expences) {
   return await AxiosConfig.post("/delete/delete-category", expences, {
