1aed8b7d2e14ce2d1c6db4c437a00d82c31847cf
Qbatz <qbatz.tech@gmail.com>
2025-02-13 09:38:12 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,15 @@
+const webpack = require("webpack");
+
+module.exports = {
+  resolve: {
+    fallback: {
+      crypto: require.resolve("crypto-browserify"),
+    },
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
+  ],
+};
 
