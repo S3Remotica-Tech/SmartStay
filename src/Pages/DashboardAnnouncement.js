e79f4b08b04c3299685ef013b75e1e02da504795
vimala rosily <vimalarosily0430@gmail.com>
2025-02-14 16:20:13 +0530

    Announcement page comment popup close issue fixed

--- src/Pages/DashboardAnnouncement.js
+++ src/Pages/DashboardAnnouncement.js
@@ -119,6 +119,9 @@ function DashboardAnnouncement(props) {
       dispatch({ type: 'CREATECOMMENTS', payload: { an_id: selectedCard, comment: Comments } })
     }
 
+    setComments(""); 
+  setShowCommentModal(false);
+
   }
 
 
@@ -137,7 +140,8 @@ function DashboardAnnouncement(props) {
         }
       })
     }
-
+    setSubComment("");
+    setShowCommentModal(false); 
   }
 
   const handleShowAnnouncement = () => {
@@ -223,7 +227,8 @@ function DashboardAnnouncement(props) {
     setShowCommentModal(false)
     setSubCommentModal(true)
     setSelectedCommentId(null);
-
+    // setCommentsList(false);
+    // setSubCommentModal(false);
   }
 
   const handleCloseTittle = () => setshowTittleModal(false);
