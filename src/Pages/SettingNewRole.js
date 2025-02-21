f9c05e897a226a190d325ad71278186124929dfd
Sivasri234 <sivadhanyasri143@gmail.com>
2025-02-21 14:55:48 +0530

    invoice_editerror

--- src/Pages/SettingNewRole.js
+++ src/Pages/SettingNewRole.js
@@ -495,33 +495,30 @@ function SettingNewRole({ hostelid }) {
 
 
       {loading &&
-        <div
-          style={{
-            position: 'absolute',
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: '200px',
-            height: "60vh",
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: 'transparent',
-            opacity: 0.75,
-            zIndex: 10,
-          }}
-        >
-          <div
-            style={{
-              borderTop: '4px solid #1E45E1',
-              borderRight: '4px solid transparent',
-              borderRadius: '50%',
-              width: '40px',
-              height: '40px',
-              animation: 'spin 1s linear infinite',
-            }}
-          ></div>
-        </div>
+         <div
+         style={{
+             position: 'fixed',
+    right: "30%",
+             display: 'flex',
+             height: "50vh",
+             alignItems: 'center',
+             justifyContent: 'center',
+             backgroundColor: 'transparent',
+             opacity: 0.75,
+             zIndex: 10,
+         }}
+     >
+         <div
+             style={{
+                 borderTop: '4px solid #1E45E1',
+                 borderRight: '4px solid transparent',
+                 borderRadius: '50%',
+                 width: '40px',
+                 height: '40px',
+                 animation: 'spin 1s linear infinite',
+             }}
+         ></div>
+     </div>
       }
 
 
