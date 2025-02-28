e8780214621a824b16badc73cc19e958715c1187
Sivasri234 <sivadhanyasri143@gmail.com>
2025-02-28 12:01:49 +0530

    general

--- src/Pages/SettingGeneral.js
+++ src/Pages/SettingGeneral.js
@@ -500,7 +500,6 @@ function SettingGeneral() {
 
   useEffect(() => {
     setLoading(true)
-    console.log("load",loading);
    
      dispatch({ type: "GETALLGENERAL" });
      const timeout = setTimeout(() => {
@@ -645,7 +644,7 @@ function SettingGeneral() {
   // };
 
   useEffect(() => {
-    if (state.Settings?.StatusCodeforGetGeneral == 200 || state.Settings?.StatusCodeforGetGeneral === 201) {
+    if (state.Settings?.StatusCodeforGetGeneral == 200 ) {
       setGeneralFilterddata(state.Settings?.settingGetGeneralData);
       setLoading(false)
 
@@ -788,46 +787,35 @@ function SettingGeneral() {
                   overflowY: "auto",}}>
 
         {loading &&
-          <div
-            // style={{
-            //   position: 'fixed',
-            //   top: 0,
-            //   right: 0,
-            //   bottom: 0,
-            //   left: '200px',
-            //   display: 'flex',
-            //   height: "50vh",
-            //   alignItems: 'center',
-            //   justifyContent: 'center',
-            //   backgroundColor: 'transparent',
-            //   opacity: 0.75,
-            //   zIndex: 10,
-            // }}
-            style={{
-              position: 'fixed',
-              top: 50,
-              right: 0,
-              bottom: 0,
-              left: 0,
-              display: 'flex',
-              alignItems: 'center',
-              // justifyContent: 'center',
-              backgroundColor: 'transparent',
-              opacity: 0.75,
-              zIndex: 10,
-            }}
-          >
-            <div
-              style={{
-                borderTop: '4px solid #1E45E1',
-                borderRight: '4px solid transparent',
-                borderRadius: '50%',
-                width: '40px',
-                height: '40px',
-                animation: 'spin 1s linear infinite',
-              }}
-            ></div>
-          </div>
+        
+
+<div
+                        style={{
+                            position: 'fixed',
+                   right: "30%",
+                            display: 'flex',
+                            height: "50vh",
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            backgroundColor: 'transparent',
+                            opacity: 0.75,
+                            zIndex: 10,
+                        }}
+                    >
+                        <div
+                            style={{
+                                borderTop: '4px solid #1E45E1',
+                                borderRight: '4px solid transparent',
+                                borderRadius: '50%',
+                                width: '40px',
+                                height: '40px',
+                                animation: 'spin 1s linear infinite',
+                            }}
+                        ></div>
+                    </div>
+
+
+
         }
 
 
