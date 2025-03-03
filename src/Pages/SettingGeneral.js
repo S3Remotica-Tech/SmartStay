e88b143a6e5bd948bdc5494da2e504d101dadee0
chitradevi08 <chitra.devi@s3remotica.com>
2025-03-03 19:59:18 +0530

    changed4

--- src/Pages/SettingGeneral.js
+++ src/Pages/SettingGeneral.js
@@ -791,28 +791,30 @@ function SettingGeneral() {
         {loading &&
            <div
            style={{
-               position: 'fixed',
-               right: "30%",
-               display: 'flex',
-               height: "50vh",
-               alignItems: 'center',
-               justifyContent: 'center',
-               backgroundColor: 'transparent',
-               opacity: 0.75,
-               zIndex: 10,
+             position: 'fixed',
+             top: '48%',
+             left: '68%',
+             transform: 'translate(-50%, -50%)',
+             width: '100vw',
+             height: '100vh',
+             display: 'flex',
+             alignItems: 'center',
+             justifyContent: 'center',
+             backgroundColor: 'transparent',
+             zIndex: 1050,
            }}
-       >
+         >
            <div
-               style={{
-                   borderTop: '4px solid #1E45E1',
-                   borderRight: '4px solid transparent',
-                   borderRadius: '50%',
-                   width: '40px',
-                   height: '40px',
-                   animation: 'spin 1s linear infinite',
-               }}
+             style={{
+               borderTop: '4px solid #1E45E1',
+               borderRight: '4px solid transparent',
+               borderRadius: '50%',
+               width: '40px',
+               height: '40px',
+               animation: 'spin 1s linear infinite',
+             }}
            ></div>
-       </div>
+         </div>
         }
 
 
