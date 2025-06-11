59697c8ee72bb22ae68c97e4d41dcff254b807cf
Mathubala <premathujasvi5706@gmail.com>
2025-06-11 15:37:21 +0530

    console removed

--- src/Pages/AssetFile/Asset.js
+++ src/Pages/AssetFile/Asset.js
@@ -40,8 +40,7 @@ function Asset() {
   const [searchQuery, setSearchQuery] = useState("");
   const [showDropDown, setShowDropDown] = useState(false)
   const [showFilterData, setShowFilterData] = useState(false)
-  const stateAccount = useSelector(state => state.createAccount)
-  const [hoveredIndex, setHoveredIndex] = useState(null);
+   const [hoveredIndex, setHoveredIndex] = useState(null);
 
   useEffect(() => {
     if (state.UsersList?.exportAssetsDetail?.response?.fileUrl) {
