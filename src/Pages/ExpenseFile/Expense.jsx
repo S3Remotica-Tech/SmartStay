74ab788aa522c3ed899c4ce0ed6b7cbf4ef9b80b
Mathubala <mathubala.p@s3remotica.com>
2025-07-16 15:56:13 +0530

    subscription validation added

--- src/Pages/ExpenseFile/Expense.jsx
+++ src/Pages/ExpenseFile/Expense.jsx
@@ -1,4 +1,4 @@
-
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState, useRef } from "react";
 import { FormControl, InputGroup, Table, Modal } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
