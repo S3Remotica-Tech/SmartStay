2a2ba15074f4f6d7f14e7bac208807c8124d5ac4
Sivasri234 <sivadhanyasri143@gmail.com>
2025-07-25 15:20:55 +0530

    booking

--- src/WebService/Config.js
+++ src/WebService/Config.js
@@ -1,7 +1,7 @@
 const config = {
-    //  apiBaseUrl: import.meta.env.VITE_API_URL
+     apiBaseUrl: import.meta.env.VITE_API_URL
     //  apiBaseUrl: "https://smartstaytestingapi.s3remotica.com",
-    apiBaseUrl: "http://localhost:2001",
+    // apiBaseUrl: "http://localhost:2001",
 
     // apiBaseUrl: "http://13.126.102.54:2001",
     // apiBaseUrl: "https://smartstaydevapi.s3remotica.com",
