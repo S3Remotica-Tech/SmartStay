eb5630b08bdaf217fc66b8ce8e158f869fdb1f56
Allwin <allwinpon@gmail.com>
2025-08-18 18:01:13 +0530

    customer page bug fixing

--- src/Pages/CustomerFile/UserListCheckoutForm.jsx
+++ src/Pages/CustomerFile/UserListCheckoutForm.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Modal, Button, Form } from "react-bootstrap";
 import "flatpickr/dist/flatpickr.css";
