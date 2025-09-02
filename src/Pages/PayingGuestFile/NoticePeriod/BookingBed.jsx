c9d67f5309cab533ff0976f63d8035703d6433f2
Sivasri234 <sivadhanyasri143@gmail.com>
2025-09-02 10:39:03 +0530

    notice_period

--- src/Pages/PayingGuestFile/NoticePeriod/BookingBed.jsx
+++ src/Pages/PayingGuestFile/NoticePeriod/BookingBed.jsx
@@ -259,29 +259,9 @@ useEffect(()=>{
 
 
 
-      const selectedOption =
-  booking_customername &&
-  state.UsersList?.Users?.find(
-    (u) => String(u.ID) === String(booking_customername)
-  );
+    
 
-const customerOptions = [
-  { value: "", label: "Select Customer" }, // default
-  ...(state.UsersList?.UnAssignCustomerDetails?.length > 0
-    ? state.UsersList.UnAssignCustomerDetails.map((u) => ({
-        value: String(u.id),
-        label: u.Name,
-      }))
-    : []),
-  ...(selectedOption
-    ? [
-        {
-          value: String(selectedOption.ID),
-          label: selectedOption.Name,
-        },
-      ]
-    : []),
-];
+
 
   return (
     <div>
