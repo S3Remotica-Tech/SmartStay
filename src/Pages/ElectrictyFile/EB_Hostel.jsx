4e948ffbb14f654a6eeeb1c31a7d30126d5da000
Sivasri234 <sivadhanyasri143@gmail.com>
2025-09-22 10:17:02 +0530

    eb

--- src/Pages/ElectrictyFile/EB_Hostel.jsx
+++ src/Pages/ElectrictyFile/EB_Hostel.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { Table } from "react-bootstrap";
 import { Modal, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -13,118 +13,153 @@ import building from '/src/Assets/Images/New_images/building1.svg';
 import Form from "react-bootstrap/Form";
 import PaginationList from "../../Components/PaginationList";
 import EB_RoomOverview from "./EB_RoomOverview";
+import { useDispatch, useSelector } from "react-redux";
+
 
 
 
 
 const RoomReadingTable = () => {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+console.log("RoomReadingTable",state)
+  useEffect(() => {
+    // props.setLoader(true)
+    if (state.login.selectedHostel_Id) {
+      dispatch({
+        type: "EBSTARTMETERLIST",
+        payload: { hostel_id: state.login.selectedHostel_Id },
+      });
+
+    }
+
+  }, [state.login.selectedHostel_Id]);
+
+
+
+  const ebList = state.PgList.EB_startmeterlist;
+
+// group and take last entry
+const latestReadings = Object.values(
+  ebList.reduce((acc, item) => {
+    const key = `${item.hostel_id}_${item.floor_id}_${item.room_id}`;
+    // If no entry yet or this one is later, replace
+    if (!acc[key] || new Date(item.date) > new Date(acc[key].date)) {
+      acc[key] = item;
+    }
+    return acc;
+  }, {})
+);
+
+// Now you have only the latest record per room
+console.log("latestReadings",latestReadings);
 
 
   const [activeTab, setActiveTab] = useState("customer");
 
-  const data = [
-    {
-      floor: "Ground floor",
-      room: "Room 001",
-      occupants: 3,
-      billingMonth: "Aug",
-      previous: 100,
-      current: 400,
-      units: 300,
-      amount: "₹0.00",
-      note: "EB reading was already entered for this room.",
-    },
-    {
-      floor: "Ground floor",
-      room: "Room 002",
-      occupants: 3,
-      billingMonth: "Aug",
-      previous: 220,
-      current: 440,
-      units: 220,
-      amount: "₹2,200",
-    },
-    {
-      floor: "Ground floor",
-      room: "Room 003",
-      occupants: 2,
-      billingMonth: "Aug",
-      previous: 270,
-      current: "---",
-      units: "---",
-      amount: "₹0.00",
-    },
-    {
-      floor: "Ground floor",
-      room: "Room 004",
-      occupants: 3,
-      billingMonth: "Aug",
-      previous: 300,
-      current: "---",
-      units: "---",
-      amount: "₹0.00",
-    },
-    {
-      floor: "Ground floor",
-      room: "Room 005",
-      occupants: 2,
-      billingMonth: "Aug",
-      previous: 120,
-      current: "---",
-      units: "---",
-      amount: "₹0.00",
-    },
-    {
-      floor: "1st floor",
-      room: "Room 101",
-      occupants: 2,
-      billingMonth: "Aug",
-      previous: 250,
-      current: "---",
-      units: "---",
-      amount: "₹0.00",
-    },
-    {
-      floor: "1st floor",
-      room: "Room 102",
-      occupants: 3,
-      billingMonth: "Aug",
-      previous: 234,
-      current: "---",
-      units: "---",
-      amount: "₹0.00",
-    },
-    {
-      floor: "1st floor",
-      room: "Room 103",
-      occupants: 3,
-      billingMonth: "Aug",
-      previous: 213,
-      current: "---",
-      units: "---",
-      amount: "₹0.00",
-    },
-    {
-      floor: "1st floor",
-      room: "Room 104",
-      occupants: 3,
-      billingMonth: "Aug",
-      previous: 220,
-      current: "---",
-      units: "---",
-      amount: "₹0.00",
-    },
-    {
-      floor: "1st floor",
-      room: "Room 105",
-      occupants: 1,
-      billingMonth: "Aug",
-      previous: 120,
-      current: "---",
-      units: "---",
-      amount: "₹0.00",
-    },
-  ];
+  // const data = [
+  //   {
+  //     floor: "Ground floor",
+  //     room: "Room 001",
+  //     occupants: 3,
+  //     billingMonth: "Aug",
+  //     previous: 100,
+  //     current: 400,
+  //     units: 300,
+  //     amount: "₹0.00",
+  //     note: "EB reading was already entered for this room.",
+  //   },
+  //   {
+  //     floor: "Ground floor",
+  //     room: "Room 002",
+  //     occupants: 3,
+  //     billingMonth: "Aug",
+  //     previous: 220,
+  //     current: 440,
+  //     units: 220,
+  //     amount: "₹2,200",
+  //   },
+  //   {
+  //     floor: "Ground floor",
+  //     room: "Room 003",
+  //     occupants: 2,
+  //     billingMonth: "Aug",
+  //     previous: 270,
+  //     current: "---",
+  //     units: "---",
+  //     amount: "₹0.00",
+  //   },
+  //   {
+  //     floor: "Ground floor",
+  //     room: "Room 004",
+  //     occupants: 3,
+  //     billingMonth: "Aug",
+  //     previous: 300,
+  //     current: "---",
+  //     units: "---",
+  //     amount: "₹0.00",
+  //   },
+  //   {
+  //     floor: "Ground floor",
+  //     room: "Room 005",
+  //     occupants: 2,
+  //     billingMonth: "Aug",
+  //     previous: 120,
+  //     current: "---",
+  //     units: "---",
+  //     amount: "₹0.00",
+  //   },
+  //   {
+  //     floor: "1st floor",
+  //     room: "Room 101",
+  //     occupants: 2,
+  //     billingMonth: "Aug",
+  //     previous: 250,
+  //     current: "---",
+  //     units: "---",
+  //     amount: "₹0.00",
+  //   },
+  //   {
+  //     floor: "1st floor",
+  //     room: "Room 102",
+  //     occupants: 3,
+  //     billingMonth: "Aug",
+  //     previous: 234,
+  //     current: "---",
+  //     units: "---",
+  //     amount: "₹0.00",
+  //   },
+  //   {
+  //     floor: "1st floor",
+  //     room: "Room 103",
+  //     occupants: 3,
+  //     billingMonth: "Aug",
+  //     previous: 213,
+  //     current: "---",
+  //     units: "---",
+  //     amount: "₹0.00",
+  //   },
+  //   {
+  //     floor: "1st floor",
+  //     room: "Room 104",
+  //     occupants: 3,
+  //     billingMonth: "Aug",
+  //     previous: 220,
+  //     current: "---",
+  //     units: "---",
+  //     amount: "₹0.00",
+  //   },
+  //   {
+  //     floor: "1st floor",
+  //     room: "Room 105",
+  //     occupants: 1,
+  //     billingMonth: "Aug",
+  //     previous: 120,
+  //     current: "---",
+  //     units: "---",
+  //     amount: "₹0.00",
+  //   },
+  // ];
 
   const [showModal, setShowModal] = useState(false);
   const [selectedRow, setSelectedRow] = useState(null);
@@ -142,6 +177,7 @@ const RoomReadingTable = () => {
   };
 
   const handleActionClick = (row) => {
+    console.log("item",row)
     setSelectedRow(row);
     setShowModal(true);
   };
@@ -258,28 +294,30 @@ const RoomReadingTable = () => {
                 </tr>
               </thead>
               <tbody style={{ fontSize: 14, color: "#000" }}>
+                
                 <PaginationList>
-                  {data.map((row, i) => (
+                    {latestReadings.map((item, i) => (
+                  // {data.map((row, i) => (
                     <tr key={i} style={{ borderBottom: "1px solid #ddd", height: "50px" }}>
-                      <td style={{ fontSize: 15, fontWeight: 600 }}>{row.floor}</td>
+                      <td style={{ fontSize: 15, fontWeight: 600 }}>{item.floor_name}</td>
                       <td
                         style={{ color: "#1E45E1", cursor: "pointer", fontWeight: 500 }}
-                        onClick={() => handleRoomDetailsPage(row.room)}
+                        onClick={() => handleRoomDetailsPage(item.Room_Id)}
                       >
-                        {row.room}
+                        {item.Room_Id}
                       </td>
-                      <td style={{ paddingLeft: "40px" }}>{row.occupants}</td>
-                      <td style={{ paddingLeft: "40px" }}>{row.billingMonth}</td>
-                      <td style={{ paddingLeft: "40px" }}>{row.previous}</td>
-                      <td style={{ paddingLeft: "40px" }}>{row.current}</td>
-                      <td style={{ paddingLeft: "40px" }}>{row.units}</td>
-                      <td style={{ paddingLeft: "30px" }}>{row.amount}</td>
+                      <td style={{ paddingLeft: "40px" }}>{item.occupants}</td>
+                      <td style={{ paddingLeft: "40px" }}>{item.date}</td>
+                      <td style={{ paddingLeft: "40px" }}>{item.previous}</td>
+                      <td style={{ paddingLeft: "40px" }}>{item.reading}</td>
+                      <td style={{ paddingLeft: "40px" }}>{item.total_reading}</td>
+                      <td style={{ paddingLeft: "30px" }}>{item.total_amount}</td>
                       <td style={{ paddingLeft: "40px" }}>
                         <img
                           src={Group}
                           alt="action"
                           style={{ cursor: "pointer" }}
-                          onClick={() => handleActionClick(row)}
+                          onClick={() => handleActionClick(item)}
                         />
                       </td>
                     </tr>
@@ -345,10 +383,10 @@ const RoomReadingTable = () => {
                     fontWeight: 600,
                   }}
                 >
-                  {selectedRow.room}
+                  {selectedRow.Room_Id}
                   <div className="d-flex justify-content-start align-items-center" style={{ gap: 6, marginTop: 4 }}>
                     <img src={building} height="14" width="14" alt="Ground Floor" />
-                    <div style={{ color: "#4B4B4B", fontSize: 12 }}>{selectedRow.floor}</div>
+                    <div style={{ color: "#4B4B4B", fontSize: 12 }}>{selectedRow.floor_name}</div>
                   </div>
                 </span>
               </div>
