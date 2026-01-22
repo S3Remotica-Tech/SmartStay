66c12a0dec70c8dcc23f5b41b3a10920c63af036
Mathubala1995 <jasvika5706@gmail.com>
2026-01-22 11:04:57 +0530

    update

--- src/Pages/PayingGuestFile/PgList.jsx
+++ src/Pages/PayingGuestFile/PgList.jsx
@@ -915,9 +915,9 @@ function PgList() {
             {
               !state.login.isTrigger &&
 
-              <div className="d-flex justify-content-between align-items-center mb-3">
+              <div className="d-flex justify-content-between align-items-center mb-3 ">
 
-                <div className="d-flex align-items-center">
+                <div className="d-flex align-items-center ">
 
 
 
@@ -935,9 +935,7 @@ function PgList() {
                 </div>
 
                 <div className="d-flex justify-content-between align-items-center">
-                  <div className="me-3">
-
-                  </div>
+                 
 
                   <div style={{ marginTop: 5 }}>
                     <Button
