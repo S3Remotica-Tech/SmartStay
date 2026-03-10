af16e66c5a058438e45b7155ab9f1e3b684ef7c3
priyarajanharshini <priyamaha0707@gmail.com>
2026-03-10 17:34:36 +0530

    FinalPagination

--- src/Components/PaginationList.jsx
+++ src/Components/PaginationList.jsx
@@ -1,4 +1,224 @@
- /* eslint-disable react-hooks/exhaustive-deps */
+//  /* eslint-disable react-hooks/exhaustive-deps */
+// import React, { useState, useEffect } from "react";
+// import PropTypes from "prop-types";
+
+// function PaginationList({
+//   display,
+//   onPageChange,
+//   itemsPerPage = 10,
+//   children,
+// }) {
+//   const totalItems = React.Children.count(children);
+//   const totalPages = Math.max(1, Math.ceil(totalItems / itemsPerPage));
+
+//   const [currentPage, setCurrentPage] = useState(1);
+
+//   useEffect(() => {
+//     if (currentPage > totalPages) setCurrentPage(totalPages);
+//   }, [totalPages]);
+
+//   const handleChangePage = (page) => {
+//     const p = Math.max(1, Math.min(page, totalPages));
+//     setCurrentPage(p);
+//      onPageChange?.(p);
+//   };
+
+
+//   const getPages = () => {
+//     const pages = [];
+//     const delta = 1;
+
+//     pages.push(1);
+
+//     if (currentPage - delta > 2) pages.push("...");
+
+//     for (
+//       let i = Math.max(2, currentPage - delta);
+//       i <= Math.min(totalPages - 1, currentPage + delta);
+//       i++
+//     ) {
+//       pages.push(i);
+//     }
+
+//     if (currentPage + delta < totalPages - 1) pages.push("...");
+
+//     if (totalPages > 1) pages.push(totalPages);
+
+//     return pages;
+//   };
+
+//   const startIndex = (currentPage - 1) * itemsPerPage;
+//   const endIndex = startIndex + itemsPerPage;
+//   const paginatedChildren = React.Children.toArray(children).slice(
+//     startIndex,
+//     endIndex
+//   );
+
+//   if (totalItems === 0) return null;
+
+//   // console.log("display",display)
+
+//   return (
+//     <>
+
+
+
+//       {paginatedChildren}
+
+
+//       {totalPages > 1 && (
+//         <div
+//           style={{
+//             position: "fixed",
+//             bottom: 0,
+//             left: display ? "50%" : "30%",
+//             width: "100%",
+//             maxWidth: display ? "500px" : "700px",
+//             background: "#FFFF",
+//             padding: "0px 16px",
+//             display: "flex",
+//             justifyContent: "start",
+//             alignItems: "center",
+//             gap: 24,
+//             zIndex: 10,
+//             fontFamily: "Gilroy",
+//             // boxShadow: !display && "0 -4px 12px rgba(0, 0, 0, 0.05)",
+//           }}
+//         >
+
+//           <div
+//             style={{
+//               fontSize: 16,
+//               color: "#222222",
+//               fontWeight: 500,
+//               fontFamily: "Gilroy"
+//             }}
+//           >
+//             Total Records :  {children?.length}
+//           </div>
+
+
+
+//           <div style={{
+//             // position: "absolute",
+//             // left: "50%",
+//             // transform: "translateX(-50%)",
+//           }}>
+//             <ul
+//               style={{
+//                 display: "flex",
+//                 alignItems: "center",
+//                 gap: "25px",
+//                 listStyle: "none",
+//                 margin: 0,
+//                 padding: 0, fontFamily: "Gilroy"
+//                 // justifyContent: "center"
+//               }}
+//             >
+
+//               <li>
+//                 <button
+//                   onClick={() => handleChangePage(currentPage - 1)}
+//                   disabled={currentPage === 1}
+//                   style={{
+//                     fontFamily: "Gilroy",
+//                     padding: "6px 14px",
+//                     borderRadius: "8px",
+//                     border: "1px solid #E5E7EB",
+//                     backgroundColor: "#E5F0FF",
+//                     color:
+//                       currentPage === 1 ? "#111827" : "#1D4ED8",
+
+//                     cursor: currentPage === 1 ? "not-allowed" : "pointer",
+//                     fontSize: 14,
+//                     fontWeight: 600,
+//                     opacity: currentPage === 1 ? "0.5" : "1",
+//                   }}
+//                 >
+//                   ← Previous
+//                 </button>
+//               </li>
+
+
+//               {getPages().map((page, index) => (
+//                 <li key={index}>
+//                   {page === "..." ? (
+//                     <span
+//                       style={{
+//                         padding: "6px 10px",
+//                         color: "#9CA3AF",
+//                         fontSize: 14, fontFamily: "Gilroy"
+//                       }}
+//                     >
+//                       …
+//                     </span>
+//                   ) : (
+//                     <button
+//                       onClick={() => handleChangePage(page)}
+//                       style={{
+//                         width: 32, fontFamily: "Gilroy",
+//                         height: 32,
+//                         borderRadius: "8px",
+//                         border: "none",
+//                         fontSize: 14,
+//                         fontWeight: 600,
+//                         cursor: "pointer",
+//                         background:
+//                           page === currentPage ? "#1E45E1" : "transparent",
+//                         color:
+//                           page === currentPage ? "#fff" : "#374151",
+//                       }}
+//                     >
+//                       {page}
+//                     </button>
+//                   )}
+//                 </li>
+//               ))}
+
+
+//               <li>
+//                 <button
+//                   onClick={() => handleChangePage(currentPage + 1)}
+//                   disabled={currentPage === totalPages}
+//                   style={{
+//                     fontFamily: "Gilroy",
+//                     padding: "6px 14px",
+//                     borderRadius: "8px",
+//                     border: "1px solid #E5E7EB",
+//                     backgroundColor: "#E5F0FF",
+//                     color:
+//                       currentPage === totalPages ? "#111827" : "#1D4ED8",
+//                     cursor:
+//                       currentPage === totalPages
+//                         ? "not-allowed"
+//                         : "pointer",
+//                     fontSize: 14,
+//                     fontWeight: 600, opacity: currentPage === totalPages ? "0.5" : "1",
+//                   }}
+//                 >
+//                   Next →
+//                 </button>
+//               </li>
+//             </ul>
+//           </div>
+//         </div>
+//       )}
+
+//     </>
+//   );
+// }
+
+// PaginationList.propTypes = {
+//   onPageChange: PropTypes.func,
+//   itemsPerPage: PropTypes.number,
+//   children: PropTypes.node,
+//   display: PropTypes.bool
+// };
+
+// export default PaginationList;
+
+
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
@@ -9,9 +229,12 @@ function PaginationList({
   children,
 }) {
   const totalItems = React.Children.count(children);
-  const totalPages = Math.max(1, Math.ceil(totalItems / itemsPerPage));
 
   const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(itemsPerPage);
+  const [options, setOptions] = useState([10, 20, 50]);
+
+  const totalPages = Math.max(1, Math.ceil(totalItems / pageSize));
 
   useEffect(() => {
     if (currentPage > totalPages) setCurrentPage(totalPages);
@@ -20,9 +243,8 @@ function PaginationList({
   const handleChangePage = (page) => {
     const p = Math.max(1, Math.min(page, totalPages));
     setCurrentPage(p);
-     onPageChange?.(p);
+    onPageChange?.(p);
   };
-
 
   const getPages = () => {
     const pages = [];
@@ -47,8 +269,9 @@ function PaginationList({
     return pages;
   };
 
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+
   const paginatedChildren = React.Children.toArray(children).slice(
     startIndex,
     endIndex
@@ -56,154 +279,103 @@ function PaginationList({
 
   if (totalItems === 0) return null;
 
-  // console.log("display",display)
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1440) {
+        setOptions([20, 40, 60, 80]);
+      } else {
+        setOptions([10, 20, 50]);
+      }
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <>
-
-
-
       {paginatedChildren}
 
+      {totalItems > 10 && (
 
-      {totalPages > 1 && (
         <div
-          style={{
-            position: "fixed",
-            bottom: 0,
-            left: display ? "50%" : "30%",
-            width: "100%",
-            maxWidth: display ? "500px" : "700px",
-            background: "#FFFF",
-            padding: "0px 16px",
-            display: "flex",
-            justifyContent: "start",
-            alignItems: "center",
-            gap: 24,
-            zIndex: 10,
-            fontFamily: "Gilroy",
-            // boxShadow: !display && "0 -4px 12px rgba(0, 0, 0, 0.05)",
-          }}
+          className={`fixed bottom-1 left-0 right-0 lg:left-[260px] lg:right-4 md:left-[260px] md:right-16 
+  w-auto bg-white px-2 py-1 md:gap-24 lg:gap-96
+  flex items-center z-10 font-[Gilroy]`}
         >
+          <div className="flex items-center gap-3 flex-shrink-0 border-0">
+            <div className="text-sm text-[#222222] font-medium">
+              Total Records : {totalItems}
+            </div>
 
-          <div
-            style={{
-              fontSize: 16,
-              color: "#222222",
-              fontWeight: 500,
-              fontFamily: "Gilroy"
-            }}
-          >
-            Total Records :  {children?.length}
-          </div>
-
-
-
-          <div style={{
-            // position: "absolute",
-            // left: "50%",
-            // transform: "translateX(-50%)",
-          }}>
-            <ul
-              style={{
-                display: "flex",
-                alignItems: "center",
-                gap: "25px",
-                listStyle: "none",
-                margin: 0,
-                padding: 0, fontFamily: "Gilroy"
-                // justifyContent: "center"
+            <select
+              value={pageSize}
+              onChange={(e) => {
+                setPageSize(Number(e.target.value));
+                setCurrentPage(1);
               }}
+              className="border border-[#E5E7EB] rounded-[6px] px-2 py-1 w-[70px] text-sm font-[Gilroy] cursor-pointer"
             >
-
-              <li>
-                <button
-                  onClick={() => handleChangePage(currentPage - 1)}
-                  disabled={currentPage === 1}
-                  style={{
-                    fontFamily: "Gilroy",
-                    padding: "6px 14px",
-                    borderRadius: "8px",
-                    border: "1px solid #E5E7EB",
-                    backgroundColor: "#E5F0FF",
-                    color:
-                      currentPage === 1 ? "#111827" : "#1D4ED8",
-
-                    cursor: currentPage === 1 ? "not-allowed" : "pointer",
-                    fontSize: 14,
-                    fontWeight: 600,
-                    opacity: currentPage === 1 ? "0.5" : "1",
-                  }}
-                >
-                  ← Previous
-                </button>
-              </li>
-
-
-              {getPages().map((page, index) => (
-                <li key={index}>
-                  {page === "..." ? (
-                    <span
-                      style={{
-                        padding: "6px 10px",
-                        color: "#9CA3AF",
-                        fontSize: 14, fontFamily: "Gilroy"
-                      }}
-                    >
-                      …
-                    </span>
-                  ) : (
-                    <button
-                      onClick={() => handleChangePage(page)}
-                      style={{
-                        width: 32, fontFamily: "Gilroy",
-                        height: 32,
-                        borderRadius: "8px",
-                        border: "none",
-                        fontSize: 14,
-                        fontWeight: 600,
-                        cursor: "pointer",
-                        background:
-                          page === currentPage ? "#1E45E1" : "transparent",
-                        color:
-                          page === currentPage ? "#fff" : "#374151",
-                      }}
-                    >
-                      {page}
-                    </button>
-                  )}
-                </li>
+              {options.map((opt) => (
+                <option key={opt} value={opt}>
+                  {opt}
+                </option>
               ))}
-
-
-              <li>
-                <button
-                  onClick={() => handleChangePage(currentPage + 1)}
-                  disabled={currentPage === totalPages}
-                  style={{
-                    fontFamily: "Gilroy",
-                    padding: "6px 14px",
-                    borderRadius: "8px",
-                    border: "1px solid #E5E7EB",
-                    backgroundColor: "#E5F0FF",
-                    color:
-                      currentPage === totalPages ? "#111827" : "#1D4ED8",
-                    cursor:
-                      currentPage === totalPages
-                        ? "not-allowed"
-                        : "pointer",
-                    fontSize: 14,
-                    fontWeight: 600, opacity: currentPage === totalPages ? "0.5" : "1",
-                  }}
-                >
-                  Next →
-                </button>
-              </li>
-            </ul>
+            </select>
           </div>
+
+          <ul className="flex items-center gap-3 sm:gap-5 list-none m-0 p-0 border-0 ml-auto">
+            <li>
+              <button
+                onClick={() => handleChangePage(currentPage - 1)}
+                disabled={currentPage === 1}
+                className={`py-[4px] px-[10px] rounded-[6px] text-[15px] font-semibold
+        ${currentPage === 1
+                    ? "cursor-not-allowed opacity-50"
+                    : "cursor-pointer"
+                  }`}
+              >
+                &lt;
+              </button>
+            </li>
+
+            {getPages().map((page, index) => (
+              <li key={index}>
+                {page === "..." ? (
+                  <span className="px-[8px] text-[#9CA3AF] text-sm">…</span>
+                ) : (
+                  <button
+                    onClick={() => handleChangePage(page)}
+                    className={`w-7 h-7 rounded-[6px] text-sm font-semibold
+            ${page === currentPage
+                        ? "bg-[#1E45E1] text-white"
+                        : "bg-transparent text-[#374151]"
+                      }`}
+                  >
+                    {page}
+                  </button>
+                )}
+              </li>
+            ))}
+
+            <li>
+              <button
+                onClick={() => handleChangePage(currentPage + 1)}
+                disabled={currentPage === totalPages}
+                className={`py-[4px] px-[10px] rounded-[6px] text-[15px] font-semibold
+        ${currentPage === totalPages
+                    ? "cursor-not-allowed opacity-50"
+                    : "cursor-pointer"
+                  }`}
+              >
+                &gt;
+              </button>
+            </li>
+          </ul>
         </div>
       )}
-
     </>
   );
 }
@@ -212,8 +384,7 @@ PaginationList.propTypes = {
   onPageChange: PropTypes.func,
   itemsPerPage: PropTypes.number,
   children: PropTypes.node,
-  display: PropTypes.bool
+  display: PropTypes.bool,
 };
 
 export default PaginationList;
-
