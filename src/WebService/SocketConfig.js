409c7ccad7c2059147fc017f3ccb5401a12cc417
priyarajanharshini <priyamaha0707@gmail.com>
2026-04-06 11:38:17 +0530

    Socket Url

--- src/WebService/SocketConfig.js
+++ src/WebService/SocketConfig.js
@@ -1,10 +1,11 @@
 import SockJS from "sockjs-client";
 import { over } from "stompjs";
+import ConfigV2 from './ConfigV2';
  
 let stompClient = null;
 // https://webdevapi.qbatz.com/ws
 export const Connect = (onMessageReceived, paymentId) => {
-    const socket = new SockJS("https://webdevapi.qbatz.com/ws");
+    const socket = new SockJS(ConfigV2.apiBaseUrl + "/ws");
     stompClient = over(socket);
  
     console.log(paymentId)
