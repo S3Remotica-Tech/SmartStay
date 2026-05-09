f09f69e60fe1978b16d912d042c001aecce51d32
Mathubala1995 <jasvika5706@gmail.com>
2026-05-09 15:57:30 +0530

    Update

--- src/Pages/Bills/RecordPayment.jsx
+++ src/Pages/Bills/RecordPayment.jsx
@@ -323,7 +323,7 @@ function RecordPayment({ show, handleClose, selectedUserId, invoiceList }) {
 
           <Modal.Body>
             <>
-              <div className="d-flex align-items-center gap-2 ">
+              <div className="flex items-center gap-2  bg-[#F7F9FF] px-4 py-2 rounded">
                 {profile_pic ? (
                   <img
                     src={profile_pic && profile_pic !== "0" && profile_pic}
@@ -820,33 +820,11 @@ function RecordPayment({ show, handleClose, selectedUserId, invoiceList }) {
             </div>
           ) : null}
 
-          {formRecordLoading && (
-            <div
-              style={{
-                position: "absolute",
-                top: "50%",
-                left: "50%",
-                transform: "translate(-50%, -50%)",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                backgroundColor: "transparent",
-                opacity: 0.75,
-                zIndex: 10,
-              }}
-            >
-              <div
-                style={{
-                  borderTop: "4px solid #1E45E1",
-                  borderRight: "4px solid transparent",
-                  borderRadius: "50%",
-                  width: "40px",
-                  height: "40px",
-                  animation: "spin 1s linear infinite",
-                }}
-              ></div>
-            </div>
-          )}
+        {formRecordLoading && (
+  <div className="absolute top-1/2 left-1/2 z-10 flex items-center justify-center -translate-x-1/2 -translate-y-1/2 opacity-75">
+    <div className="w-10 h-10 border-t-4 border-r-4 border-t-[#1E45E1] border-r-transparent rounded-full animate-spin"></div>
+  </div>
+)}
 
           <Modal.Footer style={{ border: "none" }}>
             <div className="text-end mt-4">
