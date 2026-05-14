059c28b2d6b3cdf58d1f3aaec40555d7a4eacfdf
Mathubala1995 <jasvika5706@gmail.com>
2026-05-14 13:37:02 +0530

    Update

--- src/Pages/CustomerFile/FinalSettlement.jsx
+++ src/Pages/CustomerFile/FinalSettlement.jsx
@@ -569,7 +569,7 @@ function FinalSettlement() {
 
   return (
     <div className="h-screen overflow-y-hidden ">
-      <div className="mb-3 sticky bg-white z-0 h-[50px] px-3 py-2.5 ">
+      <div className="mb-3 sticky top-0 bg-white z-0 h-[50px] px-3 py-2.5 ">
         <div className="flex items-center gap-3">
           <ArrowLeft
             onClick={handleClose}
