54d4460bb46491b0cdc9c91b2e7fa531e77215c5
Mathubala1995 <jasvika5706@gmail.com>
2026-05-21 16:07:18 +0530

    Update

--- src/Pages/PayingGuestFile/DeleteBed.jsx
+++ src/Pages/PayingGuestFile/DeleteBed.jsx
@@ -20,8 +20,6 @@ function DeleteBed({ show, handleClose, deleteBedDetails }) {
 
   const [actionType, setActionType] = useState("addCustomer");
   const [showAddCustomer, setShowAddCustomer] = useState(false);
-  // const [rolePermission, setRolePermission] = useState("");
-  // const [customerDeletePermission, setCustomerDeletePermission] = useState("")
   const [advanceForm, setAdvanceForm] = useState(false);
   const [user_details, setUserDetails] = useState("");
   const { bed } = deleteBedDetails;
@@ -31,25 +29,9 @@ function DeleteBed({ show, handleClose, deleteBedDetails }) {
   const [advanceDateError, setAdvanceDateError] = useState("");
   const [advanceDueDateError, setAdvanceDueDateError] = useState("");
 
-  // useEffect(() => {
-  //   setRolePermission(state.createAccount.accountList);
-  // }, [state.createAccount.accountList]);
-
-  // useEffect(() => {
-  //   if (
-  //     rolePermission[0]?.is_owner === 1 ||
-  //     rolePermission[0]?.role_permissions[4]?.per_delete === 1
-  //   ) {
-  //     setCustomerDeletePermission("");
-  //   } else {
-  //     setCustomerDeletePermission("Permission Denied");
-  //   }
-  // }, [rolePermission]);
-
   const handleDeleteBed = () => {
     dispatch({ type: "CLEAR_DELETE_BED_ERROR" });
     if (deleteBedDetails?.bed.id) {
-      setFormLoading(true);
       dispatch({
         type: "DELETEBED",
         payload: {
@@ -59,6 +41,7 @@ function DeleteBed({ show, handleClose, deleteBedDetails }) {
           bedId: bed.id,
         },
       });
+      setFormLoading(true);
     }
   };
 
@@ -79,137 +62,6 @@ function DeleteBed({ show, handleClose, deleteBedDetails }) {
 
   const handleShow = (type) => {
     setActionType(type);
-  };
-
-  const handleSaveCustomer = () => {
-    dispatch({ type: "CLEAR_PHONE_ERROR" });
-    dispatch({ type: "CLEAR_EMAIL_ERROR" });
-
-    const joining = moment(user_details.joining_date);
-    const invoice = moment(advanceDate);
-    const due = moment(advanceDueDate);
-
-    if (invoice.isBefore(joining, "day")) {
-      setAdvanceDateError("Before joining date not allowed");
-      hasError = true;
-    }
-
-    if (due.isBefore(invoice, "day")) {
-      setAdvanceDueDateError("Due date cannot be before invoice date");
-      hasError = true;
-    }
-
-    const filterData_Hostel_Name =
-      state.UsersList?.hostelListNewDetails?.data?.filter(
-        (view) => view.id === user_details.Hostel_Id,
-      );
-
-    let hasError = false;
-
-    if (!advanceDate) {
-      setAdvanceDateError("Please Select Invoice Date");
-      hasError = true;
-    } else {
-      setAdvanceDateError("");
-    }
-
-    if (!advanceDueDate) {
-      setAdvanceDueDateError("Please Select Due Date");
-      hasError = true;
-    } else {
-      setAdvanceDueDateError("");
-    }
-
-    if (!filterData_Hostel_Name || filterData_Hostel_Name.length === 0) {
-      hasError = true;
-    }
-
-    if (hasError) {
-      return;
-    }
-    const incrementDateAndFormat = (date) => {
-      return moment(date).add(1, "day").format("YYYY-MM-DD");
-    };
-
-    const mobileNumber = `${user_details.countryCode}${user_details.phone}`;
-    const formattedSelectedDate = incrementDateAndFormat(
-      user_details.joining_date,
-    );
-    const formattedAdvanceDate = incrementDateAndFormat(advanceDate);
-    const formattedAdvanceDateDue = incrementDateAndFormat(advanceDueDate);
-
-    dispatch({
-      type: "ADDUSER",
-      payload: {
-        profile: user_details.profile,
-        firstname: user_details.firstname.trim(),
-        lastname: user_details.lastname.trim(),
-        Phone: mobileNumber,
-        Email: user_details.Email,
-        hostel_Id: user_details.Hostel_Id,
-        Floor: user_details.Floor,
-        Rooms: user_details.Rooms,
-        Bed: user_details.Bed,
-        Address: user_details.Address,
-        area: user_details.area,
-        landmark: user_details.landmark,
-        city: user_details.city,
-        pincode: user_details.pincode,
-        state: user_details.state,
-        HostelName: filterData_Hostel_Name[0]?.Name,
-        AdvanceAmount: user_details.AdvanceAmount,
-        RoomRent: user_details.RoomRent,
-        joining_date: formattedSelectedDate,
-        isadvance: 1,
-        invoice_date: formattedAdvanceDate,
-        due_date: formattedAdvanceDateDue,
-        reasons: user_details.reasons,
-      },
-    });
-    setFormLoading(true);
-  };
-
-  const handleCancelCustomer = () => {
-    const filterData_Hostel_Name =
-      state.UsersList?.hostelListNewDetails?.data?.filter(
-        (view) => view.id === user_details.Hostel_Id,
-      );
-    const incrementDateAndFormat = (date) => {
-      return moment(date).add(1, "day").format("YYYY-MM-DD");
-    };
-
-    const mobileNumber = `${user_details.countryCode}${user_details.phone}`;
-    const formattedSelectedDate = incrementDateAndFormat(
-      user_details.joining_date,
-    );
-
-    dispatch({
-      type: "ADDUSER",
-      payload: {
-        profile: user_details.profile,
-        firstname: user_details.firstname.trim(),
-        lastname: user_details.lastname.trim(),
-        Phone: mobileNumber,
-        Email: user_details.Email,
-        hostel_Id: user_details.Hostel_Id,
-        Floor: user_details.Floor,
-        Rooms: user_details.Rooms,
-        Bed: user_details.Bed,
-        Address: user_details.Address,
-        area: user_details.area,
-        landmark: user_details.landmark,
-        city: user_details.city,
-        pincode: user_details.pincode,
-        state: user_details.state,
-        HostelName: filterData_Hostel_Name[0]?.Name,
-        AdvanceAmount: user_details.AdvanceAmount,
-        RoomRent: user_details.RoomRent,
-        joining_date: formattedSelectedDate,
-        isadvance: 0,
-        reasons: user_details.reasons,
-      },
-    });
-    setFormLoading(true);
   };
 
   useEffect(() => {
@@ -269,204 +121,33 @@ function DeleteBed({ show, handleClose, deleteBedDetails }) {
             Cancel
           </Button>
 
-          {/* {actionType === 'addCustomer' && (
-      <Button
-        className="!w-[130px] !h-[52px] !rounded-lg !border !border-[#1E45E1] !bg-[#1E45E1] !text-white !text-sm !font-semibold !font-gilroy"
-        disabled={customerAddPermission}
-        onClick={handleAddCustomer}
-      >
-        Add Customer
-      </Button>
-    )} */}
-
           <Button
-            className="!w-[130px] !h-[52px] !rounded-lg !border !border-[#1E45E1] !bg-[#1E45E1] !text-white !text-sm !font-semibold !font-gilroy"
             disabled={formLoading}
             onClick={handleDeleteBed}
+            className={`
+    !w-[130px]
+    !h-[52px]
+    !rounded-lg
+    !border
+    !border-[#1E45E1]
+    !bg-[#1E45E1]
+    !text-white
+    !text-sm
+    !font-semibold
+    !font-gilroy
+    ${formLoading ? "!opacity-70 !cursor-not-allowed" : ""}
+  `}
           >
-            Delete
+            {formLoading ? (
+              <div className="flex items-center justify-center gap-2">
+                <div className="w-4 h-4 border-2 border-white border-t-transparent rounded-full animate-spin" />
+                Deleting...
+              </div>
+            ) : (
+              "Delete"
+            )}
           </Button>
         </Modal.Footer>
-      </Modal>
-
-      <Modal
-        show={advanceForm}
-        onHide={handleCloseAdvanceForm}
-        backdrop="static"
-        centered
-      >
-        <Modal.Dialog
-          style={{
-            maxWidth: 666,
-            paddingRight: "10px",
-            borderRadius: "30px",
-          }}
-          className="m-0 p-0"
-        >
-          <Modal.Header style={{ position: "relative" }}>
-            <div
-              style={{
-                fontSize: 20,
-                fontWeight: 600,
-                fontFamily: "Gilroy",
-              }}
-            >
-              Generate Advance
-            </div>
-
-            <CloseCircle
-              size="24"
-              color="#000"
-              onClick={handleCloseAdvanceForm}
-              style={{ cursor: "pointer" }}
-            />
-          </Modal.Header>
-          <Modal.Body style={{ paddingTop: 2 }}>
-            <div className="d-flex align-items-center">
-              <div className="container">
-                <div className="row mb-3">
-                  <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                    <Form.Group className="mb-2" controlId="checkoutDate">
-                      <Form.Label
-                        style={{
-                          fontSize: 14,
-                          color: "#222222",
-                          fontFamily: "Gilroy",
-                          fontWeight: 500,
-                        }}
-                      >
-                        Invoice Date{" "}
-                        <span style={{ color: "red", fontSize: "20px" }}>
-                          *
-                        </span>
-                      </Form.Label>
-
-                      <div
-                        className="datepicker-wrapper"
-                        style={{ position: "relative", width: "100%" }}
-                      >
-                        <DatePicker
-                          style={{
-                            width: "100%",
-                            height: 48,
-                            cursor: "pointer",
-                            fontFamily: "Gilroy",
-                          }}
-                          format="DD/MM/YYYY"
-                          placeholder="DD/MM/YYYY"
-                          value={advanceDate ? dayjs(advanceDate) : null}
-                          onChange={(date) => {
-                            setAdvanceDateError("");
-                            setAdvanceDate(date ? date.toDate() : null);
-                          }}
-                          getPopupContainer={(triggerNode) =>
-                            triggerNode.closest(".datepicker-wrapper")
-                          }
-                          dropdownClassName="custom-datepicker-popup"
-                          disabledDate={(current) =>
-                            current && current > dayjs().endOf("day")
-                          }
-                        />
-                      </div>
-                    </Form.Group>
-                    {advanceDateError && (
-                      <ErrorMessage message={advanceDateError} type="error" />
-                    )}
-                  </div>
-                  <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                    <Form.Group className="mb-2" controlId="checkoutDate">
-                      <Form.Label
-                        style={{
-                          fontSize: 14,
-                          color: "#222222",
-                          fontFamily: "Gilroy",
-                          fontWeight: 500,
-                        }}
-                      >
-                        Due Date{" "}
-                        <span style={{ color: "red", fontSize: "20px" }}>
-                          *
-                        </span>
-                      </Form.Label>
-
-                      <div
-                        className="datepicker-wrapper"
-                        style={{ position: "relative", width: "100%" }}
-                      >
-                        <DatePicker
-                          style={{
-                            width: "100%",
-                            height: 48,
-                            cursor: "pointer",
-                            fontFamily: "Gilroy",
-                          }}
-                          format="DD/MM/YYYY"
-                          placeholder="DD/MM/YYYY"
-                          value={advanceDueDate ? dayjs(advanceDueDate) : null}
-                          onChange={(date) => {
-                            setAdvanceDueDateError("");
-                            setAdvanceDueDate(date ? date.toDate() : null);
-                          }}
-                          getPopupContainer={(triggerNode) =>
-                            triggerNode.closest(".datepicker-wrapper")
-                          }
-                          dropdownClassName="custom-datepicker-popup"
-                        />
-                      </div>
-                    </Form.Group>
-                    {advanceDueDateError && (
-                      <ErrorMessage
-                        message={advanceDueDateError}
-                        type="error"
-                      />
-                    )}
-                  </div>
-                </div>
-
-                <div className="row col-md-12 col-lg-12">
-                  <div className="col-md-6 col-lg-6">
-                    <Button
-                      variant="secondary"
-                      className="w-100"
-                      style={{
-                        height: 45,
-                        borderRadius: 12,
-                        fontSize: 15,
-                        fontWeight: 500,
-                        fontFamily: "Montserrat",
-                        paddingLeft: 20,
-                        paddingRight: 20,
-                      }}
-                      onClick={handleCancelCustomer}
-                    >
-                      Cancel
-                    </Button>
-                  </div>
-
-                  <div className="col-md-6 col-lg-6 mb-2">
-                    <Button
-                      variant="primary"
-                      className="w-100"
-                      style={{
-                        backgroundColor: "#1E45E1",
-                        height: 45,
-                        borderRadius: 12,
-                        fontSize: 15,
-                        fontWeight: 600,
-                        fontFamily: "Montserrat",
-                        paddingLeft: 25,
-                        paddingRight: 25,
-                      }}
-                      onClick={handleSaveCustomer}
-                    >
-                      Save
-                    </Button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Modal.Body>
-        </Modal.Dialog>
       </Modal>
 
       {showAddCustomer && (
