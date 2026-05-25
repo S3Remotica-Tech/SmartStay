5f053b1cccedd801ce9f7250979af27120a2ec00
Mathubala1995 <jasvika5706@gmail.com>
2026-05-25 15:36:34 +0530

    Update

--- src/Pages/CustomerFile/UserListInvoice.jsx
+++ src/Pages/CustomerFile/UserListInvoice.jsx
@@ -23,6 +23,7 @@ import RefundAmount from "../Bills/RefundAmount";
 import UnPaidInvoice from "../Bills/UnPaidInvoice";
 import DiscountInvoice from "../PDF/DiscountInvoice";
 import PermissionDeniedMessage from "../../Utils/PermissionDeniedMessage";
+import NoDataMessage from "../../Utils/NoDataMessage";
 
 function UserListInvoice(props) {
   const state = useSelector((state) => state);
@@ -334,10 +335,14 @@ function UserListInvoice(props) {
           </>
         ) : invoiceFilterddata?.length > 0 ? (
           <>
-            <div className="relative flex flex-col h-[calc(100vh-355px)] mt-10 overflow-y-scroll overflow-x-auto show-scrolls pb-1">
-              <div className="flex-1  ">
-                <table className="min-w-full border-collapse w-full font-gilroy text-gray-900 text-sm font-medium">
-                  <thead className="bg-blue-100 sticky top-0 z-20">
+            <div className="bg-white   rounded-xl shadow-sm border border-[#E8E8E8] mx-1 my-4 ">
+              <div
+                id="tableContainer"
+                // ref={tableContainerRef}
+                className="overflow-auto relative  h-[calc(100vh-140px)]  rounded-xl show-scrolls"
+              >
+                <table className=" w-full font-gilroy">
+                  <thead className="bg-[#F9FAFB] sticky top-0 z-40 text-[#6B7280] text-xs uppercase">
                     <tr className="h-9">
                       <th className="w-[230px] px-2 whitespace-nowrap">
                         Invoice number
@@ -660,20 +665,7 @@ function UserListInvoice(props) {
             </div>
           </>
         ) : (
-          <div className="mt-2.5 flex justify-center">
-            <div>
-              <div className="2xl:mt-24 text-center">
-                <img src={Emptystate} alt="emptystate" />
-              </div>
-
-              <div className="pb-1 text-center font-semibold text-[16px] text-[#4B4B4B] font-gilroy">
-                No Bills available
-              </div>
-              <div className="pb-1 text-center font-medium text-[14px] text-[#4B4B4B] font-gilroy">
-                There are no Bills added.
-              </div>
-            </div>
-          </div>
+          <NoDataMessage label="Invoice" isHeightChanged={true} />
         )}
       </div>
     </>
