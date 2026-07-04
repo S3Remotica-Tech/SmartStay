2eba4e97963916b6173894dae6217c540d6058b4
Mathubala1995 <jasvika5706@gmail.com>
2026-07-04 12:32:55 +0530

    updated

--- src/Pages/CustomerFile/TenantActions.jsx
+++ src/Pages/CustomerFile/TenantActions.jsx
@@ -46,10 +46,7 @@ function TenantActions({ show, handleClose }) {
 
   return (
     <>
-      <div
-        className="fixed inset-0  z-[9999] bg-black/40"
-        onClick={handleClose}
-      />
+      <div className="fixed inset-0  z-50 bg-black/40" onClick={handleClose} />
 
       <div className="fixed inset-0 z-[9999] flex items-center justify-center p-2">
         <div
